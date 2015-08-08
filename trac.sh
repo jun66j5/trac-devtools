c62ddfa264d57a6c6e4dd5950ad31c714dde0c5a
Jun Omae <jun66j5@gmail.com>
2015-08-08 20:08:41 +0900

    Added modwsgi command

--- trac.sh
+++ trac.sh
@@ -22,26 +22,34 @@ if [ ! -x "$venv/bin/python" ]; then
     exit 1
 fi
 
+cmd="$1"
+shift 2
+if [ "$cmd" != python -a $# = 0 ]; then
+    echo "Requires a directory of Trac Environment" 1>&2
+    exit 1
+fi
+
+tmpdir=
+
+_cleanup() {
+    if [ "x$tmpdir" != x -a -d "$tmpdir" ]; then
+        rm -rf "$tmpdir"
+    fi
+}
+
 _dirname() {
     local progname="$0"
     if [ -h "$progname" ]; then
         progname="`readlink -f \"$progname\"`"
     fi
-    echo "`dirname \"$progname\"`/"
+    local dir="`dirname \"$progname\"`"
+    [ "$dir" = . ] && dir="$PWD"
+    [ "$dir" != / ] && dir="$dir/"
+    echo "$dir"
     return 0
 }
 
-type="$1"
-shift 2
-case "$type" in
-admin|server|nginx)
-    if [ $# = 0 ]; then
-        echo "Requires a directory of Trac Environment" 1>&2
-        exit 1
-    fi
-    ;;
-esac
-case "$type" in
+case "$cmd" in
 admin)
     exec "$venv/bin/trac-admin" "$@"
     ;;
@@ -54,6 +62,21 @@ nginx)
     nginx -c "$conf"
     trap "nginx -c \"$conf\" -s stop" INT
     TRAC_ENV="$1" "$venv/bin/uwsgi" -s 127.0.0.1:3001 -w trac.web.main:dispatch_request
+    ;;
+modwsgi)
+    if [ ! -d "$venv/lib/python2.7" ]; then
+        echo "Require python2.7, $venv is using `cd \"$venv\"/lib && echo python2.?`."
+        exit 1
+    fi
+    tmpdir=`mktemp -d /dev/shm/modwsgi-XXXXXX`
+    trap _cleanup 0 1 2 3 15
+    echo 'from trac.web.main import dispatch_request as application' >$tmpdir/trac.wsgi
+    _PWD="`_dirname`"
+    _VENVDIR="$venv"
+    _TMPDIR="$tmpdir"
+    TRAC_ENV="$1"
+    export _PWD _VENVDIR _TMPDIR TRAC_ENV
+    /usr/sbin/apache2 -X -f "${_PWD}modwsgi.conf"
     ;;
 python)
     exec "$venv/bin/python"
