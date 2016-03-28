4b44f12dcdc860fba2e48d100b5a0d96d58490a2
Jun Omae <jun66j5@gmail.com>
2016-03-28 12:35:21 +0900

    Build python bindings with subversion 1.8.8 on Ubuntu 1404

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 set -e
 arcdir="$HOME/arc"
 venvroot="$HOME/venv"
+svnver=1.8.8
 
 LC_ALL=en_US.UTF8
 TMP=/dev/shm
@@ -14,7 +15,7 @@ if [ $# -eq 0 ]; then
         py27-0.11 py26-0.11 py25-0.11 py24-0.11 \
         py27-0.12 py26-0.12 py25-0.12 py24-0.12 \
         py27-1.0  py26-1.0  py25-1.0 \
-        py27-1.1  py26-1.1  py25-1.1
+        py27-1.1  py26-1.1
 fi
 
 for i in "$@"; do
@@ -27,53 +28,64 @@ for i in "$@"; do
     esac
     venvdir="$venvroot/$i"
     rm -rf "$venvdir"
-    /usr/bin/virtualenv -p "$python" --no-site-packages --unzip-setuptools \
-        --never-download "$venvdir"
+    case "$i" in
+    py2[45]*)
+        rm -rf "$TMP/virtualenv-1.7.2"
+        tar xzf "$arcdir/virtualenv-1.7.2.tar.gz" -C "$TMP"
+        PYTHONPATH="$TMP/virtualenv-1.7.2" "$python" -m virtualenv \
+            -p "$python" --unzip-setuptools --never-download "$venvdir"
+        rm -rf "$TMP/virtualenv-1.7.2"
+        ;;
+    *)
+        /usr/bin/virtualenv \
+            -p "$python" --unzip-setuptools --never-download "$venvdir"
+        ;;
+    esac
     python="$venvdir/bin/python"
 
     case "$i" in
     py2?)
         "$venvdir/bin/pip" install -q --download-cache="$HOME/arc/pip" \
-            mercurial MySQL-python docutils pytz twill==0.9.1 uWSGI
+            MySQL-python docutils pytz twill==0.9.1 uWSGI
         case "$i" in
         py24)
             "$venvdir/bin/pip" install -q --download-cache="$HOME/arc/pip" \
                 psycopg2==2.4.6 Babel==0.9.6 configobj==4.7.2 coverage==3.7.1 \
-                'pysqlite<2.8.0' 'lxml<3.4.0dev' 'Pygments<2.0dev'
+                mercurial==3.4.2 'pysqlite<2.8.0' 'lxml<3.4.0dev' \
+                'Pygments<2.0dev'
             ;;
         py25)
             "$venvdir/bin/pip" install -q --download-cache="$HOME/arc/pip" \
                 psycopg2==2.5.2 Babel==0.9.6 configobj==4.7.2 coverage==3.7.1 \
-                'pysqlite<2.8.0' 'lxml<3.4.0dev' 'Pygments<2.0dev'
+                mercurial==3.4.2 'pysqlite<2.8.0' 'lxml<3.4.0dev' \
+                'Pygments<2.0dev'
             ;;
         py26)
             "$venvdir/bin/pip" install -q --download-cache="$HOME/arc/pip" \
                 psycopg2 Babel Jinja2 configobj coverage lxml Pygments \
-                'pysqlite<2.8.0' memory_profiler psutil
+                mercurial 'pysqlite<2.8.0' memory_profiler psutil
             ;;
         py27)
             "$venvdir/bin/pip" install -q --download-cache="$HOME/arc/pip" \
                 psycopg2 Babel Jinja2 configobj coverage lxml Pygments \
-                pysqlite html2rest sphinx memory_profiler psutil
+                mercurial pysqlite html2rest sphinx memory_profiler psutil
             ;;
         esac
 
-        rm -rf "$TMP/swig-1.3.40"
-        tar xzf "$arcdir/swig-1.3.40.tar.gz" -C "$TMP"
-        (cd "$TMP/swig-1.3.40" &&
-            PYTHON="$python" ./configure --with-python="$python" &&
-            make)
-        rm -rf "$TMP/subversion-1.6.23"
-        tar xjf "$arcdir/subversion-1.6.23.tar.bz2" -C "$TMP"
-        (cd "$TMP/subversion-1.6.23" &&
-            PYTHON="$python" "$python" gen-make.py --installed-libs \
-                libsvn_client,libsvn_delta,libsvn_diff,libsvn_fs,libsvn_fs_base,libsvn_fs_fs,libsvn_fs_util,libsvn_ra,libsvn_ra_local,libsvn_ra_serf,libsvn_ra_svn,libsvn_repos,libsvn_subr,libsvn_wc,libsvn_ra_neon &&
-            PYTHON="$python" ./configure --prefix="$venvdir" --with-swig="$TMP/swig-1.3.40/swig" &&
-            make swig-py &&
-            make install-swig-py &&
-            mv "$venvdir"/lib/svn-python/* "$venvdir"/lib/python2.?/site-packages/)
+        rm -rf "$TMP/subversion-$svnver"
+        tar xjf "$arcdir/subversion-$svnver.tar.bz2" -C "$TMP"
+        (
+            cd "$TMP/subversion-$svnver"
+            sed -i -e 's/-0x2050000}$/-0x2040000}/' build/find_python.sh
+            /usr/bin/python2.7 gen-make.py \
+                --installed-libs $(cd /usr/lib/x86_64-linux-gnu &&
+                                   echo libsvn_*.la | sed -e 's/-[^-]*\.la//g; s/ /,/g')
+            PYTHON="$python" ./configure --prefix="$venvdir" --with-swig="/usr/bin/swig"
+            make swig-py install-swig-py
+            mv -v "$venvdir"/lib/svn-python/* "$venvdir"/lib/python2.?/site-packages/
+        )
         "$python" -c 'from svn import core'
-        rm -rf "$TMP/swig-1.3.40" "$TMP/subversion-1.6.23"
+        rm -rf "$TMP/subversion-$svnver"
         ;;
 
     py2?-*.*)
